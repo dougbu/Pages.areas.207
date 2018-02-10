37884c5b6a288bd9524ceb9992793e1cef1abfc2
Doug Bunting <dougbu@microsoft.com>
2018-02-09 19:13:46 -0800

    Initial files - `dotnet new razor --output Pages.areas.207 --auth Individual --use-local-db`

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pages.areas._207.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
